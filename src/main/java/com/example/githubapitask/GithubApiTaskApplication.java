ef09d372a3be5505f7b6e54245b99d84d0419d4a
loudsheep <loudsheep05@gmail.com>
2025-07-20 11:28:11 +0200

    chore: init project

---
+++ src/main/java/com/example/githubapitask/GithubApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubApiTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubapitask/GithubApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubapitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
