598e72c38286cc77bac40a4ab4abaecf43f682ce
loudsheep <loudsheep05@gmail.com>
2025-07-21 09:43:29 +0200

    fix: path variable annotation in github controller

--- src/main/java/com/example/githubapitask/controllers/GithubController.java
+++ src/main/java/com/example/githubapitask/controllers/GithubController.java
@@ -3,6 +3,7 @@ package com.example.githubapitask.controllers;
 import com.example.githubapitask.dto.RepositoryDto;
 import com.example.githubapitask.services.GithubService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class GithubController {
     }
 
     @GetMapping("/{username}/repos")
-    public List<RepositoryDto> getRepositories(String username) {
+    public List<RepositoryDto> getRepositories(@PathVariable String username) {
         return githubService.getNonForkRepositories(username);
     }
 }
